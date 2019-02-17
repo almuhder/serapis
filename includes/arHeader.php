c63e88233e7fe36312b050d67f86d22456ab09b1
Majd Eddin <almuhder@gmail.com>
2019-02-17 16:52:52 +0200

    (ADD) Arabic Update

--- includes/arHeader.php
+++ includes/arHeader.php
@@ -9,10 +9,17 @@
   <meta name="Majd Eddin" content="Serapis Website">
   <link rel="shortcut icon" type="image/png" href="favicon.png" />
 
-  <title>Serapis Bootcamp | English & Digital Literacy</title>
+  <title> المعسكر التدريبي سيرابيس | اللغة الإنجليزية و المعرفة الرقمية</title>
+
 
   <!-- Bootstrap core CSS -->
-  <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+<link 
+  rel="stylesheet"
+  href="https://cdn.rtlcss.com/bootstrap/v4.2.1/css/bootstrap.min.css"
+  integrity="sha384-vus3nQHTD+5mpDiZ4rkEPlnkcyTP+49BhJ4wJeJunw06ZAp+wzzeBPUXr42fi8If"
+  crossorigin="anonymous">
+
+
 
 
   <!-- Custom fonts for this template -->
@@ -29,16 +36,18 @@
 <link href="https://fonts.googleapis.com/css?family=Cairo" rel="stylesheet">
 </head>
 <style>
-  body{
-    font-family: Arial, Helvetica, sans-serif;
+  *{
+    font-family: 'Cairo', sans-serif !important; 
   }
+  
+  
 </style>
-<body id="page-top">
+<body id="page-top" dir="rtl">
 
   <!-- Navigation -->
-  <nav class="navbar navbar-expand-lg navbar-dark fixed-top" id="mainNav">
+  <nav class="text-left navbar navbar-expand-lg navbar-dark fixed-top" id="mainNav">
     <div class="container">
-       <a class="navbar-brand js-scroll-trigger" href="http://rbkserapis.com">
+      <a class="navbar-brand js-scroll-trigger" href="http://rbkserapis.com">
         <img class="img img-responsive" src="img/serapis-logo.png">
       </a>
       <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive"
@@ -49,28 +58,28 @@
       <div class="collapse navbar-collapse" id="navbarResponsive">
         <ul class="navbar-nav text-uppercase ml-auto">
           <li class="nav-item">
-            <a class="nav-link js-scroll-trigger" href="index.html#home">Home</a>
+            <a class="nav-link js-scroll-trigger" href="index.html#home">الصفحة الرئيسية</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link js-scroll-trigger" href="index.html#about">About</a>
+            <a class="nav-link js-scroll-trigger" href="index.html#about">عن سيرابيس</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link js-scroll-trigger" href="index.html#portfolio">Curriculum</a>
+            <a class="nav-link js-scroll-trigger" href="index.html#portfolio">المنهاج</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link js-scroll-trigger" href="index.html#program">Program</a>
+            <a class="nav-link js-scroll-trigger" href="index.html#program">البرنامج</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link js-scroll-trigger" href="index.html#team">Team</a>
+            <a class="nav-link js-scroll-trigger" href="index.html#team">الفريق</a>
           </li>
           <li class="nav-item">
-              <a class="nav-link js-scroll-trigger" style=" text-transform:none" href="faq.html"> FAQs </a>
+              <a class="nav-link js-scroll-trigger" style=" text-transform:none" href="faq.html"> الأسئلة الأكثر شيوعا </a>
           </li>
           <li class="nav-item">
-            <a class="nav-link js-scroll-trigger" href="index.html#contact">Contact</a>
+            <a class="nav-link js-scroll-trigger" href="index.html#contact">اتصل بنا</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link float-right js-scroll-trigger"  style="font-family: 'Cairo', sans-serif;" href="arIndex.php">عربي</a>
+            <a class="nav-link float-right js-scroll-trigger" href="index.php">English</a>
           </li>
         </ul>
       </div>
@@ -81,9 +90,9 @@
     <header class="masthead">
       <div class="container">
         <div class="intro-text">
-          <div class="intro-lead-in" style="color:#C50084">Welcome To Serapis!</div>
-          <div class="intro-heading text-uppercase" style="color:#C50084">FREE 8 WEEKS' ENGLISH AND DIGITAL LITERACY BOOTCAMP</div>
-          <a class="btn btn-primary btn-xl text-uppercase js-scroll-trigger" href="apply/public">Apply Now</a>
+          <div class="intro-lead-in" style="color:#C50084">أهلا بكم في سيرابيس</div>
+          <div class="intro-heading text-uppercase" style="color:#C50084">مجاناً ٨ أسابيع من التدريب المكثف في اللغة الانجليزية و المعرفة الرقمية</div>
+          <a class="btn btn-primary btn-xl text-uppercase js-scroll-trigger" href="apply/public">سجل الآن</a>
         </div>
       </div>
     </header>
